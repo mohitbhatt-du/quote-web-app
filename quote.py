6794cfa8f4ce78bf2ad36b2021dd32d91cc56419
Mohit Bhatt <76405349+mohitbhatt-du@users.noreply.github.com>
2023-07-21 14:32:28 +0530

    Update quote.py

--- quote.py
+++ quote.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import time
 
-st.set_page_config(page_title="Random Quotes",layout="wide")
+st.set_page_config(page_title="Random Quotes")
 hide_footer = """
 
 <style>
